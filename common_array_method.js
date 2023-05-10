efd473bb79859653538ecac48caffd5207a347dd
alamgirweb11 <alamgirweb11@gmail.com>
2023-05-11 00:29:06 +0600

    add another example of map

--- common_array_method.js
+++ common_array_method.js
@@ -8,9 +8,22 @@ for(let i = 0; i<mapArr.length; i++){
 }
 
 // console.log(newMapArr)
-
 function getSquare(item) {
     return item * item
 }
 
 const newMapArr2 = mapArr.map(getSquare)
+
+// console.log(newMapArr2)
+const newMapArr3 = mapArr.map(function(item) {
+      return item * item
+})
+
+// console.log(newMapArr3)
+const newMapArr4 = mapArr.map(function(item, index, fullArray){
+       console.log(`item: ${item}, index: ${index}, fullArray: ${fullArray}`)
+})
+
+// using arrow function base on ES6
+const newMapArr5 = mapArr.map(item => item * item)
+console.log(newMapArr5)
