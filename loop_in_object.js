4fc674982817f8798c0924852533cd17fd0712f9
alamgirweb11 <alamgirweb11@gmail.com>
2023-05-28 23:52:11 +0600

    print capital and country name

--- loop_in_object.js
+++ loop_in_object.js
@@ -12,4 +12,20 @@ for(ob in obj){
 
 // object value and keys method
 const capitals = Object.values(obj)
-const country = Object.keys(obj)
+const countries = Object.keys(obj)
+
+// print only capital name
+capitals.map(function(capital) {
+   console.log('Capital is: ' + capital);
+})
+
+// ES6 syntax
+capitals.map(capital => console.log(`Capital is ${capital}`))
+
+// print only country
+countries.map(function(country) {
+   console.log('Country is: ' + country);
+})
+
+// ES6 syntax
+countries.map(country => console.log(`Country is ${country}`))
