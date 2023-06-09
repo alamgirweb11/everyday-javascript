92a0224d369b0415bb26310d91dc448e5bdd847e
alamgirweb11 <alamgirweb11@gmail.com>
2023-06-09 23:24:31 +0600

    min max method added

--- math_object.js
+++ math_object.js
@@ -4,3 +4,6 @@ console.dir(Math)
 console.log(Math.PI)
 // get power result of base number
 console.log(Math.pow(10, 2)) // result 100
+// min and max method
+console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 0))
+console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 0))
