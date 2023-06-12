cfdcf3a36fff079a9aa19da37ae3ab109efde0df
alamgirweb11 <alamgirweb11@gmail.com>
2023-06-12 23:58:26 +0600

    floor and round method example

--- math_object.js
+++ math_object.js
@@ -7,3 +7,7 @@ console.log(Math.pow(10, 2)) // result 100
 // min and max method
 console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 0))
 console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 0))
+// Math.floor take smallest number of float value
+console.log(Math.floor(10.99))
+// Math.round work base on float value
+console.log(Math.round(10.10))
