28599ebe8ae49c987d164ff8b9238b4a05fb0cdd
alamgirweb11 <alamgirweb11@gmail.com>
2023-07-18 23:26:25 +0600

    false return expression

--- and_or_operator_depth.js
+++ and_or_operator_depth.js
@@ -7,3 +7,17 @@ if(false) {
     console.log('Print it!');
  }
  
+ if('anything') {
+    console.log('Print it!');  // return true
+ }
+
+/**
+ * condition return false if use
+ * false 
+ * null
+ * NaN
+ * 0
+ * "" , '', `` 
+ * undefined
+ * !anythingTrue
+ */
