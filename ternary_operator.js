3fc1bf7a54a8c4f4765674a92b0098f7f35acff7
alamgirweb11 <alamgirweb11@gmail.com>
2023-08-15 23:55:27 +0600

    basic example of ternary operator

---
+++ ternary_operator.js
@@ -0,0 +1,11 @@
+// ternary operator with examples
+const age = 21
+// with if else condition
+if(age < 18) {
+    console.log('You are not adult!')
+ } else {
+    console.log('You are adult') // You are adult
+ }
+
+ // same example with ternary
+ console.log(age < 18 ? 'You are not Adult!' : 'You are Adult') // return You are adult
