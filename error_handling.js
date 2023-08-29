549a8c538005ee0ec91ad2f7d8ca60649d3fa28a
alamgirweb11 <alamgirweb11@gmail.com>
2023-08-29 23:56:31 +0600

    basic example of error handling

---
+++ error_handling.js
@@ -0,0 +1,11 @@
+console.aula('This is an error') // return console.aula is not a function
+try {
+    console.aula('This is an error') // return console.aula is not a function
+ }
+ 
+ try {
+    console.aula('This is an error') // return console.aula is not a function
+ } catch(err) {
+    console.log('Do anything here')
+    console.log(err)
+ }
