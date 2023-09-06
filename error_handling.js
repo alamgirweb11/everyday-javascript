060f16abdc755e82f64485becfd2f51c33051e58
alamgirweb11 <alamgirweb11@gmail.com>
2023-09-06 23:55:35 +0600

    add basic example of finally

--- error_handling.js
+++ error_handling.js
@@ -1,11 +1,38 @@
-console.aula('This is an error') // return console.aula is not a function
-try {
-    console.aula('This is an error') // return console.aula is not a function
- }
+// console.aula('This is an error') // return console.aula is not a function
+// try {
+//     console.aula('This is an error') // return console.aula is not a function
+//  }
  
- try {
-    console.aula('This is an error') // return console.aula is not a function
- } catch(err) {
-    console.log('Do anything here')
-    console.log(err)
- }
+//  try {
+//     console.aula('This is an error') // return console.aula is not a function
+//  } catch(err) {
+//     console.log('Do anything here')
+//     console.log(err)
+//  }
+
+ const age = 17;
+try {
+   if(age < 18) {
+      throw 'You are too young';
+   } else {
+      console.log('You are adult');
+   }
+} catch(err) {
+   console.log(err);
+}
+
+try {
+   throw 'An error'
+} catch(err) {
+   console.log(err);
+} finally {
+   console.log('I will run always');
+}
+
+try {
+   console.log('No error');
+} catch(err) {
+   console.log(err);
+} finally {
+   console.log('I will run always');
+}
