648722be8c554f2a00169eafc937a9f36030b8d9
alamgirweb11 <alamgirweb11@gmail.com>
2023-09-14 22:50:35 +0600

    count and  clear method example added

--- developer_console.js
+++ developer_console.js
@@ -3,3 +3,13 @@ console.dir(console)
 
 // assert use for testing purpose
 console.assert(1==1, 'Error')
+
+console.clear() // return  undefined
+console.count() // return 1
+console.count() // return 2
+console.count() // return 3
+console.count() // return 4
+console.count() // return 5
+console.count('Anything') // return Anything 1
+console.count('Anything') // return Anything 2
+console.count('Anything') // return Anything 3
