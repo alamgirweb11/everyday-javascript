e1f675fc2e53e18e78ece771d8514fded56a1199
alamgirweb11 <alamgirweb11@gmail.com>
2023-10-17 23:57:53 +0600

    console info and log

--- developer_console.js
+++ developer_console.js
@@ -22,3 +22,14 @@ console.log('Normal Console Log')
 console.error('Error Console Log')
 console.dir(console)
 console.groupEnd()
+
+// console info
+console.info('Just a info message')
+
+// console log
+console.log('A Simple Console Log')
+console.log('Hello I am %s!', 'Abir Ahmed')
+console.log('Addition of 2 + 2 is: %s', (2 + 2))
+const myName = 'Abir Ahmed';
+console.log(`My name is ${myName}`)
+console.log('%cAbir Ahmed', 'font-size: 50px; background-color: red')
