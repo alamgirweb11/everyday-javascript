c2cb88630d8920d629419e605f790f8434b2bb19
alamgirweb11 <alamgirweb11@gmail.com>
2023-10-31 23:58:15 +0600

    promise bool value

--- promise.js
+++ promise.js
@@ -10,3 +10,16 @@ const aPromise = control => {
        }, 3000)
     })
  }
+
+ aPromise(true)
+  .then(() => {
+     console.log('This is a Success') // output: This is a Success
+  })
+
+  aPromise(false)
+  .then(() => {
+     console.log('This is a Success')
+  })
+  .catch(() => {
+     console.log('This is a Failure') // output: This is a Failure
+  })
