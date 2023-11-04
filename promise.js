645987b60ea728f3571170cc051e6c41fdce187b
alamgirweb11 <alamgirweb11@gmail.com>
2023-11-04 22:53:19 +0600

    resolve reject basic example

--- promise.js
+++ promise.js
@@ -23,3 +23,31 @@ const aPromise = control => {
   .catch(() => {
      console.log('This is a Failure') // output: This is a Failure
   })
+
+  // send data to resolve or reject
+  const aPromiseWithData = control => { 
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+         if(control) {
+            resolve('Simple Success Data')
+         } else {
+            reject('Simple Error Data')
+         }
+      }, 3000)
+   })
+}
+
+// output of resolve
+aPromiseWithData(true)
+  .then((data) => {
+     console.log(data) // simple success data
+  })
+
+// output of reject
+aPromiseWithData(false)
+.then((data) => {
+   console.log(data);
+})
+.catch((err) => {
+   console.log(err); // simple error data
+})
