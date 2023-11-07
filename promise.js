32f88d28e2d5408e346a2e8180c6b5c009db53c7
alamgirweb11 <alamgirweb11@gmail.com>
2023-11-07 23:58:28 +0600

    multiple promise handling

--- promise.js
+++ promise.js
@@ -51,3 +51,30 @@ aPromiseWithData(false)
 .catch((err) => {
    console.log(err); // simple error data
 })
+
+// multiple promise handling
+const promise1 = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      if(true) {
+         resolve('Promise 1 Resolved')
+      } else {
+         reject('Promise 1 Error')
+      }
+   }, 5000)
+ })
+
+ const promise2 = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      if(true) {
+         resolve('Promise 2 Resolved')
+      } else {
+         reject('Promise 2 Error')
+      }
+   }, 5000)
+})
+
+Promise.all([promise1, promise2])
+
+Promise.all([promise1, promise2]).then((dataArr) => {
+   console.log(dataArr) // ["Promise 1 Resolved", "Promise 2 Resolved"]
+})
